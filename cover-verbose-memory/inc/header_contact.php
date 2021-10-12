b584767ae6c1055b798dcfec8259be53361f67d3
Kiefer Cervantes <90808927+kmcervantes17@users.noreply.github.com>
2021-10-12 15:51:50 +0800

    inc directory done

---
+++ cover-verbose-memory/inc/header_contact.php
@@ -0,0 +1,10 @@
+<header class="masthead mb-auto">
+        <div class="inner">
+          <h3 class="masthead-brand">Cover</h3>
+          <nav class="nav nav-masthead justify-content-center">
+            <a class="nav-link " href="index.php">Home</a>
+            <a class="nav-link " href="features.php">Features</a>
+            <a class="nav-link active" href="contacts.php">Contact</a>
+          </nav>
+        </div>
+      </header>
